d35d8d01bc1734c475b1edf9103e7690ddd1ca23
Maykol Martinez <MJoelMartinez320@gmail.com>
2025-07-07 18:16:22 -0300

    Añadido listar todos

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tarea;
 
 class TareaController extends Controller
 {
-    //
+    public function ListarTodas(Request $request){
+        $tareas = Tarea::all();
+
+        return $tareas;
+    }
+
 }
